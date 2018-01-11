b628d55234f2270e7a664969717d1396fcd66e80
Julian Winkel <winkel@neuerituale.com>
2018-01-11 20:06:37 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,118 @@
+/**
+ * COPYRIGHT NOTICE
+ *
+ * Copyright (c) 2018 Neue Rituale GbR
+ * @author Julian Winkel <code@neuerituale.com>
+ * @version 1.0.0
+ * @license MIT
+ *
+ * This file is part of the Blockchain project.
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+import Blockchain from "./Blockchain.js";
+
+export default class App {
+
+	constructor (){
+
+		// encrypter global
+		window.encrypter = sha256;
+
+		// Blockchain
+		this.blockchain = new Blockchain();
+
+		// Buttons
+		this.buttonValidate = document.getElementById('button-validate');
+		this.buttonManipulate = document.getElementById('button-manipulate');
+		this.buttonAdd = document.getElementById('button-add');
+
+		// States
+		this.stateValidate = document.getElementById('state-validate');
+		this.stateManipulate = document.getElementById('state-manipulate');
+		this.stateAdd = document.getElementById('state-add');
+
+		// Other
+		this.console = document.getElementById('console');
+		this.data = document.getElementById('data');
+
+		// Add events
+		this.events();
+
+		// Show
+		this.showBlockchain();
+	}
+
+	events(){
+
+		// Check Validation
+		this.buttonValidate.addEventListener('click', (e) => {
+
+			if(this.blockchain.isChainValid()) {
+				this.stateValidate.classList.remove('error');
+				this.stateValidate.classList.add('ok');
+				this.stateValidate.innerHTML = 'Blockchain is valid';
+			} else {
+				this.stateValidate.classList.remove('ok');
+				this.stateValidate.classList.add('error');
+				this.stateValidate.innerHTML = 'Blockchain is invalid';
+			}
+
+		});
+
+		// Add Data to Blockchain
+		this.buttonAdd.addEventListener('click', (e) => {
+
+			if(this.data.value) {
+
+				// add to blockchain
+				this.blockchain.addBlock(this.data.value);
+
+				// clear data
+				this.data.value = '';
+
+				// set state
+				this.stateAdd.innerHTML = 'data added.';
+
+				// update console
+				this.showBlockchain()
+
+			} else {
+				this.stateAdd.innerHTML = 'please insert Data.'
+			}
+
+		});
+
+		// manipulate Data
+		this.buttonManipulate.addEventListener('click', (e) => {
+
+			if(this.blockchain.length > 1) {
+
+				// Manipulate
+				this.blockchain[1].data = '********** DANGER !!!11 **********';
+
+				// state
+				this.stateManipulate.innerHTML = 'data manipulated';
+
+				// update console
+				this.showBlockchain()
+
+			} else {
+				this.stateManipulate.innerHTML = 'please insert Data before.'
+			}
+
+		});
+
+	}
+
+	/**
+	 * update Console
+	 */
+	showBlockchain(){
+		this.console.innerHTML = JSON.stringify(this.blockchain, false, 4);
+	}
+
+}
+
+// Run App
+window.app = new App();
---
+++ src/Blockchain.js
@@ -0,0 +1,114 @@
+/**
+ * COPYRIGHT NOTICE
+ *
+ * Copyright (c) 2018 Neue Rituale GbR
+ * @author Julian Winkel <code@neuerituale.com>
+ * @version 1.0.0
+ * @license MIT
+ *
+ * This file is part of the Blockchain project.
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+export class Block {
+
+	constructor(key, timestamp, data, previousHash = ''){
+		this.key = key;
+		this.timestamp = timestamp;
+		this.data = data;
+		this.previousHash = previousHash;
+		this.hash = this.calculateHash();
+	}
+
+	/**
+	 * Calculate Hash
+	 */
+	calculateHash() {
+		return window.encrypter(
+			this.key +
+			this.timestamp.toISOString() +
+			JSON.stringify(this.data).toString() +
+			this.previousHash
+		).toString();
+	}
+
+}
+
+
+export default class Blockchain extends Array{
+
+	constructor() {
+		super();
+		this.push(Blockchain.createGenesisBlock());
+	}
+
+
+	/**
+	 * Create A Genesis Block
+	 * @returns {Block}
+	 */
+	static createGenesisBlock(){
+		return new Block(0, new Date, 'Genesis Block', '0');
+	}
+
+
+	/**
+	 * Return Last Block from Blockchain
+	 * @returns {*}
+	 */
+	getLastBlock(){
+		return this[this.length - 1];
+	}
+
+
+	/**
+	 * Add a new Block to Blockchain
+	 * @param data
+	 * @returns {Block}
+	 */
+	addBlock(data){
+
+		// create Block
+		let block = new Block(
+			this.length,
+			new Date,
+			data,
+			this.getLastBlock().hash
+		);
+
+		this.push(block);
+		return block;
+	}
+
+
+	/**
+	 * Check Blockchain integrity
+	 * @returns {boolean}
+	 */
+	isChainValid() {
+
+		let previousBlock = '';
+
+		return this.every((block, i) => {
+
+			// Skip Genesis Block
+			if(i === 0) { previousBlock = block; return true; }
+
+			// Check current Block integrity
+			if (block.hash !== block.calculateHash()) {
+				return false;
+			}
+
+			// Check previous Block hash
+			if (block.previousHash !== previousBlock.hash) {
+				return false;
+			}
+
+			previousBlock = block;
+			return true;
+
+		});
+	}
+
+}
